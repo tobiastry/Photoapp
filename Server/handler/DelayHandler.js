2ddf89ffd3ee3200f176990b020030e14d021e73
Fabriece Sumuni <sfabriece@hotmail.com>
2013-10-09 22:51:00 +0200

    removed unnecessary files

--- Server/handler/DelayHandler.js
+++ Server/handler/DelayHandler.js
@@ -3,7 +3,6 @@ var util = require('./utils');
 
 exports.getDelay = function (req, res){
 	try{
-		console.log(req.get('Content-Type'));
 		var version = util.getVersion(req);
 		factory.getDelay(res, version);
 	}catch(e){
@@ -13,7 +12,6 @@ exports.getDelay = function (req, res){
 
 exports.setDelay = function (req, res){
 	try{
-		console.log(req.get('Content-Type'));
 		var version = util.getVersion(req);
 		factory.setDelay(res, version, req.params.value);
 	}catch(e){

f954bbfbe9f0d2cf2da6efa9e68fc9076ead74d6
Fabriece <sfabriece@hotmail.com>
2013-10-22 16:58:03 +0200

    working insert pictures

---
+++ Server/Model/V1Models.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+var delaySchema = new Schema({
+	version: {type: String, default: 'v1'},
+	time: {type: Number, default: 1}
+});
+
+var pictureSchema = new Schema({
+	version: {type: String, default: 'v1'},
+	thumburl: String,
+	url: String
+});
+
+exports.Delay = mongoose.model('Delay', delaySchema, 'delay');
+exports.Picture = mongoose.model('Picture', pictureSchema, 'picture');
---
+++ Server/handler/PictureHandler.js
@@ -0,0 +1,11 @@
+var factory = require('./Factory');
+var util = require('./utils');
+
+exports.insertPictures = function (req, res){
+	try{
+		var version = util.getVersion(req);
+		factory.insertPictures(res, version, req.body);
+	}catch(e){
+		res.send(500, e.message);
+	}
+}

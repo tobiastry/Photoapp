54086f5eec6915128fb55ad0c50f9d8d0955c071
GitLime <Emilius92@hotmail.com>
2013-10-30 14:08:34 +0100

    Commiter så jeg kan få lov til å pulle

---
+++ Client/src/main/java/sletting/PictureCom.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sletting;
+
+/**
+ *
+ * @author Emil
+ */
+public class PictureCom {
+    
+}

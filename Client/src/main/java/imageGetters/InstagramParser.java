5eec5708cc6e982d1a66e0c173ddac0697602811
Tobias <tobiastry@hotmail.com>
2013-11-07 08:45:42 +0100

    Update InstagramParser.java

--- Client/src/main/java/imageGetters/InstagramParser.java
+++ Client/src/main/java/imageGetters/InstagramParser.java
@@ -1,44 +1,40 @@
 package imageGetters;
 
 import java.io.InputStreamReader;
-import com.google.gson.*;
 import model.Picture;
-import getImage.UpdateThread;
+import com.google.gson.*;
 
 public class InstagramParser {
-	private  UpdateThread thread;
 	private  JsonArray jsonPictures;
-	
-	public InstagramParser(UpdateThread thread) {
-		this.thread=thread;
-	}
+	JsonObject obj;
 
-	public void parse(InputStreamReader reader) {
+	public JsonArray parse(InputStreamReader reader) {
 		JsonParser parser = new JsonParser();
-		JsonObject obj = parser.parse(reader).getAsJsonObject();
+		obj = parser.parse(reader).getAsJsonObject();
 		jsonPictures = obj.get("data").getAsJsonArray();
-		thread.setSize(jsonPictures.size());
-		
+		return jsonPictures;
 	}
-	
-	public void addToList(){
-		if(jsonPictures.size()!=0){
-			for(JsonElement j : jsonPictures) {
-				JsonObject jsonPicture = j.getAsJsonObject();
 
-				Picture picture = new Picture();
+	public String getNextUrl() {
+		JsonElement next_url = obj.get("pagination");
+		String url = next_url.getAsJsonObject().get("next_url").getAsString();
+        	return url;
+	}
 
-				JsonElement images = jsonPicture.get("images");
-				JsonElement thumbImage = images.getAsJsonObject().get("thumbnail"); //thumbnail=150*150/standard_resolution=612*612
-				String url = thumbImage.getAsJsonObject().get("url").getAsString();
-				picture.thumbUrl = url;
+	public Picture addToList(JsonElement j){
+		JsonObject jsonPicture = j.getAsJsonObject();
 
-				JsonElement largeImage = images.getAsJsonObject().get("standard_resolution");
-				String largeUrl = largeImage.getAsJsonObject().get("url").getAsString();
-				picture.largeUrl = largeUrl;
-				thread.add(picture);
-				thread.addCount();
-			}
-		}
+		Picture picture = new Picture();
+
+		JsonElement images = jsonPicture.get("images");
+		JsonElement thumbImage = images.getAsJsonObject().get("thumbnail"); //thumbnail=150*150/standard_resolution=612*612
+		String url = thumbImage.getAsJsonObject().get("url").getAsString();
+		picture.thumbUrl = url;
+
+		JsonElement largeImage = images.getAsJsonObject().get("standard_resolution");
+		String largeUrl = largeImage.getAsJsonObject().get("url").getAsString();
+		picture.largeUrl = largeUrl;
+
+		return picture;
 	}
 }

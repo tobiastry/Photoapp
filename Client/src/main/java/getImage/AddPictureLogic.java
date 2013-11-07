331a3dd515a2bfa0292c0362a0c53fbcda168486
Tobias <tobiastry@hotmail.com>
2013-11-07 08:43:38 +0100

    Update AddPictureLogic.java

--- Client/src/main/java/getImage/AddPictureLogic.java
+++ Client/src/main/java/getImage/AddPictureLogic.java
@@ -1,58 +1,102 @@
 package getImage;
 
-import model.Picture;
-import imageGetters.InstagramGetter;
-import imageGetters.TwitterGetter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import getImage.AddPictureGUI;
+import model.Picture;
+import imageGetters.*;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ListChangeListener;
-import javafx.collections.ObservableList;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+
+import javafx.concurrent.Task;
 
 public class AddPictureLogic {
-	private List<Picture> pictureList = new ArrayList<Picture>();
-	private TwitterGetter twitGetter;
-	private  InstagramGetter instaGetter;
-	private UpdateThread thread = new UpdateThread();
-	
-	public AddPictureLogic(){
-//		thread.start();
-		twitGetter = new TwitterGetter(getThread());
-		instaGetter = new InstagramGetter(getThread());
-//		getPictures("nofilter");
-	}
-	
-	public int getPictures(String tag) throws IOException {
-//		getThread().start();
-		twitGetter.getPictures(tag);
-		instaGetter.getPictures(tag);
-//		addPicturesToList();
-		return 0;
-	}
+        private ArrayList<Picture> pictureList = new ArrayList<Picture>();
+        private InstagramGetter instaGetter;
+        private TwitterGetter twitterGetter;
+        private JsonArray jsonPictures;
+        private List<JsonArray> testlist = new ArrayList<>();
+        
+        public AddPictureLogic(){
+                instaGetter = new InstagramGetter();
+                twitterGetter = new TwitterGetter();
+        }
 
-	public void addPicturesToList(){
-		twitGetter.addToList();
-		instaGetter.addToList();
-	}
+        public int getPictures(String tag) throws IOException {
+                String url = instaGetter.instagramUrl(tag);
+                testlist.add(instaGetter.findPictures(url));
+                url = instaGetter.getNextUrl();
+                System.out.println("Test "+url);
+                testlist.add(instaGetter.findPictures(url));
+            
+                testlist.add(twitterGetter.findPictures(tag));
+                return testlist.get(0).size()+testlist.get(1).size()+testlist.get(2).size();
+        }
+        
+        public void addPictureToList(JsonElement j, int t){
+            if(t==0){
+                pictureList.add(instaGetter.addToList(j));
+            }
+            if(t==1){
+                pictureList.add(instaGetter.addToList(j));
+            }
+            if(t==2){
+                pictureList.add(twitterGetter.addToList(j));
+            }
+        }
+        
+        public List<Picture> getPictureList() {
+                return pictureList;
+        }
+        
+        public void exportList() {
+                pictureList.clear();
+        }
 
-	public List<Picture> getPictureList() {
-		return thread.getList();
-	}
-
-	public UpdateThread getThread() {
-		return thread;
-	}
-
-	public void setThread(UpdateThread thread) {
-		this.thread = thread;
-	}
-
-
+        public Task addPicturesToList(final String tag) {
+                return new Task() {
+                        @Override
+                        protected Object call() throws Exception {
+                                int size=getPictures(tag);
+                                  int i=1;
+                                AddPictureGUI.addingToList=true;
+                                System.out.println("test"+testlist.size());
+                                System.out.println("size"+size);
+                                System.out.println("insta"+testlist.get(0).size());
+                                System.out.println("insta"+testlist.get(1).size());
+                                System.out.println("twitter"+testlist.get(2).size());
+                                for(int t=0;t<testlist.size();t++){
+                                    for(JsonElement j : testlist.get(t)) {
+                                        Thread.sleep(50);
+                                        addPictureToList(j,t);
+                                        updateProgress(i, size);
+                                        updateMessage(i+1+"/"+size);
+                                        i++;
+                                    }
+                                }
+                                testlist.clear();
+                                exportList();
+                                AddPictureGUI.addingToList=false;
+                                return true;
+                        }
+                        @Override protected void succeeded() {
+         super.succeeded();
+         updateMessage("-Ferdig-");
+        
+         }
+                        @Override protected void running() {
+         super.running();
+         updateMessage("Laster ned bilder");
+         }
+                       @Override protected void failed() {
+         super.failed();
+         updateMessage("Fant ingen bilder!");
+         updateProgress(0, 0);
+         }
+                };
+        }
 }
-
-
-
-

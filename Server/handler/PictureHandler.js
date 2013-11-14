73c1406657f9d22fde2647cf7b1c6788cdc5898a
Fabriece <sfabriece@hotmail.com>
2013-11-14 22:25:35 +0100

    fix delete picture working

--- Server/handler/PictureHandler.js
+++ Server/handler/PictureHandler.js
@@ -2,7 +2,6 @@ var factory = require('./Factory');
 var util = require('./utils');
 
 exports.insertPictures = function (req, res){
-
 		var version = util.getVersion(req);
 		factory.insertPictures(res, version, req.body, function(err){
 			if (err) {

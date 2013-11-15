2389d9f28bec53f599911a5baf3824587210dd17
Stian <stian.ramstad@gmail.com>
2013-11-15 01:42:23 +0100

    Changed the way thumbnails loaded

--- Client/src/main/java/removeImage/RemoveImageGUI.java
+++ Client/src/main/java/removeImage/RemoveImageGUI.java
@@ -159,7 +159,7 @@ public class RemoveImageGUI extends GridPane {
             grid.getChildren().addAll(thumbnails);
         }
 
-        pl.loadPictures(0);
+        pl.loadPictures(0, imagePerPane * 2);
 
         thumbIndex = 0;
 
@@ -190,12 +190,13 @@ public class RemoveImageGUI extends GridPane {
             if (thumbIndex > thumbnails.size() - 1) {
                 next.setDisable(true);
             }
+            //Loading the pictures for the next page
+            pl.loadPictures(thumbIndex + imagePerPane, imagePerPane);
         }
     }
 
     private void showPrevious() {
         thumbIndex -= imagePerPane;
-
         grid.getChildren().clear();
         for (int i = thumbIndex; i < ((thumbIndex) + imagePerPane); i++) {
             next.setDisable(false);
@@ -203,7 +204,6 @@ public class RemoveImageGUI extends GridPane {
         }
         if (thumbIndex == 0) {
             previous.setDisable(true);
-            thumbIndex = imagePerPane;
         }
     }
 }

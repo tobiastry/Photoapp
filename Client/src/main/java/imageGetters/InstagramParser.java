67f23ce5424124966dc2025fbfa8e0bc78417749
tobiastry <tobiastry@hotmail.com>
2013-11-29 00:55:37 +0100

    Fixed bug(not tested)

--- Client/src/main/java/imageGetters/InstagramParser.java
+++ Client/src/main/java/imageGetters/InstagramParser.java
@@ -10,16 +10,26 @@ public class InstagramParser {
     JsonObject obj;
 
     public JsonArray parse(InputStreamReader reader) {
+        JsonArray tempArray = new JsonArray();
         JsonParser parser = new JsonParser();
         obj = parser.parse(reader).getAsJsonObject();
         jsonPictures = obj.get("data").getAsJsonArray();
+        tempArray.add(jsonPictures);
         return jsonPictures;
     }
 
     public String getNextUrl() {
         JsonElement next_url = obj.get("pagination");
-        String url = next_url.getAsJsonObject().get("next_url").getAsString();
-        return url;
+        if (next_url != null) {
+            String url = next_url.getAsJsonObject().get("next_url").getAsString();
+            if (url != null) {
+                return url;
+            } else {
+                return null;
+            }
+        } else {
+            return null;
+        }
     }
 
     public Picture addToList(JsonElement j) {

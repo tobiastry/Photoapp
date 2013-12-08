c20bbe3646a5f8a846b97fe5c7f5749c0faaa761
Ramstad <stian.ramstad@gmail.com>
2013-12-08 16:51:01 +0100

    Removed som comments

--- Client/src/test/java/repository/TagComTest.java
+++ Client/src/test/java/repository/TagComTest.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package repository;
 
 import java.util.ArrayList;
@@ -12,10 +8,6 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author Stian
- */
 public class TagComTest {
     private ArrayList<String> tags;
     private TagCom instance;

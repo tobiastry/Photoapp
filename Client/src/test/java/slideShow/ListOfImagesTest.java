ce910ea0b61df61cfdfce300617a0d3e504ee76d
Delarock <mortenhovdan@hotmail.com>
2013-12-09 18:20:25 +0100

    Update ListOfImagesTest.java

--- Client/src/test/java/slideShow/ListOfImagesTest.java
+++ Client/src/test/java/slideShow/ListOfImagesTest.java
@@ -21,36 +21,23 @@ public class ListOfImagesTest{
     }
     
     
-    /*
-     * MÅ LAGES NYE TESTER TIL KLASSEN!
-     */
-   /* @Test
+    @Test
     public void test1(){
-        assertTrue(list.getImageList() instanceof String[]);
+        assertTrue(list.getImageViewList() instanceof Task);
     }
     
     @Test
     public void test2(){
-        assertTrue(ListOfImages.getImageList()[0] instanceof String);
+        assertTrue(list instanceof ListOfImages);
     }
     
     @Test
     public void test3(){
-        String imagePath1 = ListOfImages.getImageList()[0];
-        assertEquals(imagePath1, ListOfImages.getImageList()[0]);
+        assertTrue(slide instanceof Slideshow);
     }
     
     @Test
     public void test4(){
-        assertTrue(ListOfImages.getImageViewList() instanceof ImageView[]);
+        assertEquals(slide.getSlideshowObject(), slide);
     }
-    
-    @Test
-    public void test5(){
-        assertTrue(ListOfImages.getImageViewList()[0] instanceof ImageView);
-    }
-        @Test
-    public void test6(){
-        assertTrue(ListOfImages.getImageViewList()[0].getImage() instanceof Image);
-    }*/
 }
